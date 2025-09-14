d2b49ab243738484f01b5075250611bbebca9c74
Rubennaldos <albertonaldos@gmail.com>
2025-09-14 18:47:03 -0500

    cambios 3.0

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
+// src/main.tsx
+import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    {/* clave para GitHub Pages: usa /maracuya-pos-flow/ en prod y "/" en dev */}
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
